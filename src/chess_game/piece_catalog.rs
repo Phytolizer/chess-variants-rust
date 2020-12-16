f99df53f679460abf5193d2aa9732dce4bb0a5dd
onContentStop <mariorules24@gmail.com>
2020-12-15 21:16:54 -0600

    Factor out method to prepare for testing

--- src/chess_game/piece_catalog.rs
+++ src/chess_game/piece_catalog.rs
@@ -2,7 +2,7 @@ use std::{
     collections::HashMap,
     fmt::Display,
     fs::{self, File},
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Read},
 };
 
 use super::piece::Piece;
@@ -26,54 +26,59 @@ impl PieceCatalog {
             let file = path?;
             if file.file_type()?.is_file() && file.file_name().to_string_lossy().ends_with(".txt") {
                 let reader = BufReader::new(File::open(file.path())?);
-                let mut piece = Piece::new();
-                let mut line_num = 1;
-                for line in reader.lines() {
-                    let line = line?;
-                    if line.starts_with('-') {
-                        continue;
-                    } else if line.starts_with("Name") {
-                        piece.name = line
-                            .split_whitespace()
-                            .nth(1)
-                            .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
-                            .to_string();
-                    } else if line.starts_with("Image") {
-                        piece.image_key = line
-                            .split_whitespace()
-                            .nth(1)
-                            .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
-                            .to_string();
-                    } else if line.starts_with("Leap") {
-                        let (forward, left) = parse_move(line)?;
-                        piece.add_leap(forward, left);
-                    } else if line.starts_with("Kill") {
-                        let (forward, left) = parse_move(line)?;
-                        piece.add_kill(forward, left);
-                    } else if line.starts_with("Run") {
-                        let (forward, left) = parse_move(line)?;
-                        piece.add_run(forward, left);
-                    } else if line.starts_with("Special") {
-                        let special_move = line
-                            .split_whitespace()
-                            .nth(1)
-                            .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
-                            .to_string();
-                        piece.add_special(special_move)?;
-                    } else if line.starts_with("Promotion") {
-                        let promotion_piece = line
-                            .split_whitespace()
-                            .nth(1)
-                            .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
-                            .to_string();
-                        piece.promotions.push(promotion_piece);
-                    }
-                    line_num += 1;
-                }
+                let piece = Self::read_piece(reader)?;
                 self.catalog.insert(piece.name.clone(), piece);
             }
         }
         Ok(())
+    }
+
+    fn read_piece<R: Read>(reader: BufReader<R>) -> Result<Piece, crate::Error> {
+        let mut piece = Piece::new();
+        let mut line_num = 1;
+        for line in reader.lines() {
+            let line = line?;
+            if line.starts_with('-') {
+                continue;
+            } else if line.starts_with("Name") {
+                piece.name = line
+                    .split_whitespace()
+                    .nth(1)
+                    .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
+                    .to_string();
+            } else if line.starts_with("Image") {
+                piece.image_key = line
+                    .split_whitespace()
+                    .nth(1)
+                    .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
+                    .to_string();
+            } else if line.starts_with("Leap") {
+                let (forward, left) = parse_move(line)?;
+                piece.add_leap(forward, left);
+            } else if line.starts_with("Kill") {
+                let (forward, left) = parse_move(line)?;
+                piece.add_kill(forward, left);
+            } else if line.starts_with("Run") {
+                let (forward, left) = parse_move(line)?;
+                piece.add_run(forward, left);
+            } else if line.starts_with("Special") {
+                let special_move = line
+                    .split_whitespace()
+                    .nth(1)
+                    .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
+                    .to_string();
+                piece.add_special(special_move)?;
+            } else if line.starts_with("Promotion") {
+                let promotion_piece = line
+                    .split_whitespace()
+                    .nth(1)
+                    .ok_or_else(|| InvalidFormatError::new(line_num, line.clone()))?
+                    .to_string();
+                piece.promotions.push(promotion_piece);
+            }
+            line_num += 1;
+        }
+        Ok(piece)
     }
 
     pub fn get_piece(&self, piece_name: &str) -> Result<&Piece, crate::Error> {
